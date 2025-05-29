07fe7e06522d2b4526e4203abff351fcb7970f15
XavierSJohnson <xavier_johnson@berkeley.edu>
2025-05-28 19:35:55 -0700

    Update dynamicTitle.js

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const favicon = document.getElementById('favicon')
   const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const attentionMessage = 'Hire Xavier'
 
   document.addEventListener('visibilitychange', function (e) {
     const isPageActive = !document.hidden
